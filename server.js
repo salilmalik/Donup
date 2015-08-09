a83b9bb91f477fc9d305757fd0772c3462067140
Malik,Salil <salilmalik92@gmail.com>
2015-08-09 16:47:17 +0530

    url pointing to index page

--- server.js
+++ server.js
@@ -1,79 +1,62 @@
-var bodyParser = require('body-parser');
-var fs = require("fs");
-var Img = require('../models/image');
-var multipart = require('connect-multiparty');
-var multipartMiddleware = multipart();
-var jwt = require('jsonwebtoken');
-var config = require('../../config');
+// BASE SETUP
+// ======================================
 
-module.exports = function(app, express) {
+// CALL THE PACKAGES --------------------
+var express = require('express'); // call express
+var app = express(); // define our app using express
+var bodyParser = require('body-parser'); // get body-parser
+var morgan = require('morgan'); // used to see requests
+var mongoose = require('mongoose');
+var config = require('./config');
+var path = require('path');
 
-	var apiRouter = express.Router();
 
-	apiRouter.route('/')
 
-	.post(multipartMiddleware, function(req, res) {
-		console.log("POST CALLED");
-		var file = req.files.file;
-		console.log(file.name);
-		console.log("FILE PATH" + file.path);
-		console.log(file);
-		var image = new Img();
-		image.name = file.name;
-		console.log(image.name);
-		image.userID = req.body.userID
-		image.img.data = fs.readFileSync(file.path);
-		image.img.contentType = 'image/png';
-		image.save(function(err, objectToInsert) {
-			if (err) {
-				console.log(err);
-				return res.json({
-					success : false,
-					message : 'Image not saved. '
-				});
-			}
-			var objectId = objectToInsert._id;
-			console.log("objectId" + objectId);
-			res.json({
-				message : 'Image saved'
-			});
-		});
-	})
+// APP CONFIGURATION ==================
+// ====================================
+// use body parser so we can grab information from POST requests
+app.use(bodyParser.urlencoded({
+	extended : true
+}));
+app.use(bodyParser.json());
 
-	apiRouter.route('/:id')
-	// get the image with that id
-	.get(function(req, res) {
-		Img.findById(req.params.id, function(err, image) {
-			if (err)
-				res.send(err);
-			console.log("SEND BACK IMAGE: " + req.params.id);
-			console.log(image.name);
-			// return that user
-			res.json(image);
-		});
-	})
-	apiRouter.route('/:id')
-	// update the points
-	.put(function(req, res) {
-		Img.findById(req.params.id, function(err, image) {
-			if (err)
-				res.send(err);
-			console.log(image.points + "image.points");
-			// set the new user information if it exists in the request
-			image.points = image.points + 1;
-			// save the user
-			console.log(image.points + "image.points");
-			image.save(function(err) {
-				if (err)
-					res.send(err);
-				// return a message
-				res.json({
-					message : 'Points Upgraded'
-				});
-			});
 
-		});
-	})
+// configure our app to handle CORS requests
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers',
+			'X-Requested-With,content-type, Authorization');
+	next();
+});
 
-	return apiRouter;
-};
+// log all requests to the console
+app.use(morgan('dev'));
+
+
+// set static files location
+// used for requests that our frontend will make
+app.use(express.static(__dirname + '/public'));
+
+// ROUTES FOR OUR API =================
+// ====================================
+
+// API ROUTES ------------------------
+
+var apiRoutes = require('./app/routes/imageapi')(app, express);
+app.use('/api/image', apiRoutes);
+
+var apiRoutes = require('./app/routes/userapi')(app, express);
+app.use('/api', apiRoutes);
+
+// MAIN CATCHALL ROUTE ---------------
+// SEND USERS TO FRONTEND ------------
+// has to be registered after API ROUTES
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/app/index.html'));
+});
+
+// START THE SERVER
+// ====================================
+app.listen(config.port);
+console.log('Magic happens on port ' + config.port);
