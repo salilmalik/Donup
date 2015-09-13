f079984b22c723f16016f628b1fd96f378baf7ea
Malik,Salil <salilmalik92@gmail.com>
2015-09-14 00:38:29 +0530

    View, Controller and Service for Image Display.

--- public/app/services/imageService.js
+++ public/app/services/imageService.js
@@ -1,6 +1,19 @@
 var app = angular.module('donup');
-app.factory('ImageService',['$http',function($http){
+app.factory('ImageService',['$http',function($http,$scope){
      return{
-       
+       displayImage:displayImage
     };
+     function displayImage(user) {
+    return $http({
+        method : 'GET',
+        url : 'api/image/55f5c6fa36b5f47c0b14e3a1',
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+
+    }).success(function(data, status, headers, config) {
+        // success is the image binary,encoding it to base64 and bound to it
+        result = data;
+    })
+};
 }]);
