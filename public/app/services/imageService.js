7558aa198707709640afd2bfae94365f560b3beb
Malik,Salil <salilmalik92@gmail.com>
2015-09-19 10:46:56 +0530

    Added logo and favicon.ico.

--- public/app/services/imageService.js
+++ public/app/services/imageService.js
@@ -1,7 +1,6 @@
 var app = angular.module('donup');
 app.factory('ImageService',['$http',function($http,$scope){
      return{
-<<<<<<< HEAD
        getImage : getImage,
        getUserImages:getUserImages
     };
@@ -16,21 +15,6 @@ app.factory('ImageService',['$http',function($http,$scope){
             method: 'GET',
             url: '/api/image/getUserImages'+userId,
         })
-=======
-       displayImage:displayImage
->>>>>>> origin/master
-    };
-     function displayImage(user) {
-    return $http({
-        method : 'GET',
-        url : 'api/image/55f5c6fa36b5f47c0b14e3a1',
-        headers : {
-            'Content-Type' : 'application/json'
-        },
 
-    }).success(function(data, status, headers, config) {
-        // success is the image binary,encoding it to base64 and bound to it
-        result = data;
-    })
 };
 }]);
