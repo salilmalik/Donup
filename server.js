5f81900f92d32129464287e4567f5c539aa888f8
SarthakC <sarthak.chow@gmail.com>
2022-12-02 00:27:02 +0530

    randis

--- server.js
+++ server.js
@@ -2,67 +2,79 @@
 // ======================================
 
 // CALL THE PACKAGES --------------------
-var express = require('express'); // call express
+var express = require("express"); // call express
 var app = express(); // define our app using express
-var bodyParser = require('body-parser'); // get body-parser
-var morgan = require('morgan'); // used to see requests
-var mongoose = require('mongoose');
-var config = require('./config');
-var path = require('path');
+var bodyParser = require("body-parser"); // get body-parser
+var morgan = require("morgan"); // used to see requests
+var mongoose = require("mongoose");
+var config = require("./config");
+var path = require("path");
 
 // APP CONFIGURATION ==================
 // ====================================
 // use body parser so we can grab information from POST requests
-app.use(bodyParser.urlencoded({
-	extended : true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
 // configure our app to handle CORS requests
-app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', '*');
-	res.setHeader('Access-Control-Allow-Headers',
-			'X-Requested-With,content-type, Authorization');
-	next();
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Authorization"
+  );
+  next();
 });
 
 // log all requests to the console
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // set static files location
 // used for requests that our frontend will make
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // ROUTES FOR OUR API =================
 // ====================================
 
 // API ROUTES ------------------------
 
-var apiRoutes = require('./app/routes/imageapi')(app, express);
-app.use('/api/image', apiRoutes);
+var apiRoutes = require("./app/routes/imageapi")(app, express);
+app.use("/api/image", apiRoutes);
 
-var apiRoutes = require('./app/routes/userapi')(app, express);
-app.use('/api', apiRoutes);
+var apiRoutes = require("./app/routes/userapi")(app, express);
+app.use("/api", apiRoutes);
 
 // MAIN CATCHALL ROUTE ---------------
 // SEND USERS TO FRONTEND ------------
 // has to be registered after API ROUTES
-app.get('*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/public/app/index.html'));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/app/index.html"));
 });
-mongoose.connect('mongodb://donupapp:kuchbhi77@ds039860.mongolab.com:39860/donup');
+mongoose.connect(
+  "mongodb://donupapp:kuchbhi77@ds039860.mongolab.com:39860/donup"
+);
 
-mongoose.connection.on("open", function(ref) {
+mongoose.connection.on("open", function (ref) {
   console.log("Connected to mongo server.");
-
 });
 
-mongoose.connection.on("error", function(err) {
+mongoose.connection.on("error", function (err) {
   console.log("Could not connect to mongo server!");
   return console.log(err);
 });
 // START THE SERVER
 // ====================================
 app.listen(config.port);
-console.log('Magic happens on port ' + config.port);
+console.log("Magic happens on port " + config.port);
+
+// _____        ___   __   _   _____   _
+// |  _  \      /   | |  \ | | |  _  \ | |
+// | |_| |     / /| | |   \| | | | | | | |
+// |  _  /    / / | | | |\   | | | | | | |
+// | | \ \   / /  | | | | \  | | |_| | | |
+// |_|  \_\ /_/   |_| |_|  \_| |_____/ |_|
